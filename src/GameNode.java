047d9dd1f235989ebca6ed5ef09f43969391c774
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-20 10:23:56 +0200

    Implement children, isLosingNode and isWinningNode functions

--- src/GameNode.java
+++ src/GameNode.java
@@ -18,11 +18,27 @@ public class GameNode {
 	
 	// generate child nodes with boards for all possible moves after the current move.
 	ArrayList<GameNode> children() {
-		
+		ArrayList<GameNode> boardChildren = new ArrayList<GameNode>();
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 3; j++) {
+				int [] pos = {i, j};
+				if(!board.isEmpty(pos))
+					continue;
+				Board childBoard = board.deepClone();
+				childBoard.setMark(pos, nextMoverMark);
+				GameNode childNode = new GameNode(childBoard, (nextMoverMark == 'X')? 'O':'X', pos);
+				boardChildren.add(childNode);
+			}
+		}
+		return boardChildren;
 	}
 	
 	boolean isLosingNode(Character evaluator) {
-		 
+		Character opponentEvaluator = (evaluator == 'X')? 'O' : 'X';
+		if(board.isOver()) {  // a tie or won
+			if(board.winner() == opponentEvaluator)
+				return true;
+			return false;   // tie is (!loss) 
 		}
 		
 		ArrayList<GameNode> children = children();
@@ -41,6 +57,22 @@ public class GameNode {
 	}
 	
 	boolean isWinningNode(Character evaluator) {
+		if(board.isOver()) {                     // a tie or won
+			return board.winner() == evaluator; 
+		}
 		
+		ArrayList<GameNode> children = children();
+		boolean isWinning;
+		if(nextMoverMark == evaluator) {    // player's turn
+			isWinning = false;
+			for(GameNode child: children) 
+				isWinning |= child.isWinningNode(evaluator);
+		}
+		else {                            // opponent turn
+			isWinning = true;
+			for(GameNode child: children)
+				isWinning &= child.isWinningNode(evaluator);
+		}
+		return isWinning;
 	}
 }
