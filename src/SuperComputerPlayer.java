3e18c60811baff183a7646c735297d00b417835e
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-20 10:54:51 +0200

    Throw exception when game can't be won or tied

--- src/SuperComputerPlayer.java
+++ src/SuperComputerPlayer.java
@@ -6,13 +6,23 @@ public class SuperComputerPlayer extends ComputerPlayer{
 	
 	public int [] move(Game game, Character mark) {
 		GameNode currentNode = new GameNode(game.board, mark);
+		boolean moveToTie = false;
 		GameNode tieNode = null;
 		for(GameNode childNode: currentNode.children()) {
 			if(childNode.isWinningNode(mark))
 				return childNode.prevMovePos;
 			if(!childNode.isLosingNode(mark)) {   // the child node gives tie
 				tieNode = childNode;
+				moveToTie = true;
 			}
+		}
+		try {
+			if(!moveToTie)
+				throw new RuntimeException("Game is Not perfect! you can not win or make a draw");	
+		}
+		catch (RuntimeException e){
+			System.out.println(e);
+            System.exit(0); // Terminate JVM
 		}
 		return tieNode.prevMovePos;
 	}
