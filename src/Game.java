747ac4b154ded43b447377b1894d43ecb0cc36c2
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-20 17:06:00 +0200

    Add initializePlayers() to choose players types

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,58 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Game {
 	
 	Board board;
 	HashMap<Character, Player> players;
+	Player currentPlayer;
 	Character turn;
 	
-	public Game (Player player1, Player player2){
+	public Game (){
 	    board = new Board();
 	    players = new HashMap<>();
-	    players.put('X', player1);
-	    players.put('O', player2);
+	    initializePlayers();
 	    turn = 'X';
+	}
+	
+	private void initializePlayers() {
+		System.out.println("Welcome to the Amazing Tic Tac Toe!\n");
+		Character [] symbols = new Character[] {'X', 'O'};
+		for(Character symbol : symbols) {
+			int type = getPlayerType(symbol);
+			String name = getPlayerName(symbol);
+			Player player;
+			switch(type) {
+			case 1:
+				player = new HumanPlayer(name);
+				break;
+			case 2:
+				player = new ComputerPlayer(name);
+				break;
+			default:
+				player = new SuperComputerPlayer(name);
+			}
+			players.put(symbol, player);
+		}		
+	}
+	
+	private int getPlayerType(Character symbol) {
+		System.out.println("Please select type of "+ symbol +" player:");
+		System.out.println("1 for human player\n2 for computer player\n3 for super computer player");
+		Scanner sc = new Scanner(System.in);
+		int type = sc.nextInt();
+		return type;
+	}
+	
+	private String getPlayerName(Character symbol) {
+		System.out.println("What is " + symbol + " player name");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.nextLine();
+		return name;
 	}
 
 	void run(){
+		
 	    while(!board.isOver()){
 	        playTurn();
 	    }
@@ -30,6 +68,7 @@ public class Game {
 
 	void show(){
 	    // not very pretty printing..
+		System.out.println("\n" + currentPlayer.name + "'s turn..\n");
 	    for( Character [] row : board.rows){
 	        for(Character mark : row){
 	            System.out.print(mark + " ");
@@ -38,20 +77,21 @@ public class Game {
 	    }
 	}
 	
-	private boolean placeMark(int [] pos, Character mark){
+	private boolean placeMark (int [] pos, Character mark) throws IllegalArgumentException {
 	    try{
 	        board.setMark(pos, mark);      // throws exception when cell is not empty
 	        return true;
 	    }
 	    catch (IllegalArgumentException e){
-	    	System.out.println(e);
+	    	System.out.println(e.getMessage());
 	        return false;
 	    }
 	}
 
 	private void playTurn(){
 	    while(true){
-	        Player currentPlayer = players.get(turn);
+	        currentPlayer = players.get(turn);
+	        show();
 	        int [] pos = currentPlayer.move(this, turn);
 	        if(placeMark(pos, turn))
 	        	break;
@@ -62,9 +102,7 @@ public class Game {
 	}
 	
 	public static void main(String[] args) {
-		Player scp = new SuperComputerPlayer("aya");
-		Player hp = new HumanPlayer("ali");
-		Game game = new Game(scp, hp);
+		Game game = new Game();
 		game.run();
 	}
 
