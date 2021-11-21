bdbde5e6aba58292f5cd95469f8c2a512a586979
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-21 10:54:40 +0200

    Refactor run, playTurn and placeMark methods structure to add GUI functionalities

--- src/Game.java
+++ src/Game.java
@@ -7,14 +7,77 @@ public class Game {
 	HashMap<Character, Player> players;
 	Player currentPlayer;
 	Character turn;
+	BoardView boardView;
 	
-	public Game (){
+	public Game (Player p1, Player p2){
 	    board = new Board();
 	    players = new HashMap<>();
-	    initializePlayers();
 	    turn = 'X';
+	    boardView = new BoardView(this);
+//	    initializePlayers();               // -----------edit---------------
+		players.put('X', p1);
+		players.put('O', p2);
+	    newTurn();
 	}
 	
+	private int getPlayerType(Character symbol) {
+		System.out.println("Please select type of "+ symbol +" player:");
+		System.out.println("1 for human player\n2 for computer player\n3 for super computer player");
+		try(Scanner sc = new Scanner(System.in)){
+			int type = sc.nextInt();
+			return type;
+		}
+	}
+	
+	private String getPlayerName(Character symbol) {
+		System.out.println("What is " + symbol + " player name");
+		try(Scanner sc = new Scanner(System.in)){
+			String name = sc.nextLine();
+			return name;
+		}
+	}
+
+	void newTurn(){
+	    if(board.isWon()){
+	        Player winningPlayer = players.get(board.winner());
+	        boardView.viewText("Congratulations " + winningPlayer.name +", you won the game!");
+	    }
+	    else if(board.isTied()){
+	    	boardView.viewText("No one wins!");
+	    }
+	    else {
+	    	boardView.viewText(turn + "'s turn");
+	    	currentPlayer = players.get(turn);
+	    	if(!(currentPlayer instanceof HumanPlayer))
+	    		playComputerTurn();
+	    }
+	}
+	
+	boolean placeBoardMark (int [] pos, Character mark) throws IllegalArgumentException {
+	    try{
+	        board.setMark(pos, mark);      // throws exception when cell is not empty
+	        return true;
+	    }
+	    catch (IllegalArgumentException e){
+	    	boardView.viewText(e.getMessage());
+	        return false;
+	    }
+	}
+	
+	void swapTurns() {
+	    turn = (turn == 'X')? 'O' : 'X';
+	    newTurn();
+	}
+
+	private void playComputerTurn(){
+        currentPlayer = players.get(turn);
+        int [] pos = currentPlayer.move(this, turn);
+        if(placeBoardMark(pos, turn)) {
+        	boardView.placeButtonMark(pos, turn);
+        	swapTurns();
+        }
+	}
+
 	private void initializePlayers() {
 		System.out.println("Welcome to the Amazing Tic Tac Toe!\n");
 		Character [] symbols = new Character[] {'X', 'O'};
@@ -36,74 +99,8 @@ public class Game {
 		}		
 	}
 	
-	private int getPlayerType(Character symbol) {
-		System.out.println("Please select type of "+ symbol +" player:");
-		System.out.println("1 for human player\n2 for computer player\n3 for super computer player");
-		Scanner sc = new Scanner(System.in);
-		int type = sc.nextInt();
-		return type;
-	}
-	
-	private String getPlayerName(Character symbol) {
-		System.out.println("What is " + symbol + " player name");
-		Scanner sc = new Scanner(System.in);
-		String name = sc.nextLine();
-		return name;
-	}
-
-	void run(){
-		
-	    while(!board.isOver()){
-	        playTurn();
-	    }
-
-	    if(board.isWon()){
-	        Player winningPlayer = players.get(board.winner());
-	        System.out.println("Congratulations " + winningPlayer.name +", you won the game!");
-	    }
-	    else {
-	        System.out.println("No one wins!");
-	    }
-	}
-
-	void show(){
-	    // not very pretty printing..
-		System.out.println("\n" + currentPlayer.name + "'s turn..\n");
-	    for( Character [] row : board.rows){
-	        for(Character mark : row){
-	            System.out.print(mark + " ");
-	        }
-	        System.out.println();
-	    }
-	}
-	
-	private boolean placeMark (int [] pos, Character mark) throws IllegalArgumentException {
-	    try{
-	        board.setMark(pos, mark);      // throws exception when cell is not empty
-	        return true;
-	    }
-	    catch (IllegalArgumentException e){
-	    	System.out.println(e.getMessage());
-	        return false;
-	    }
-	}
-
-	private void playTurn(){
-	    while(true){
-	        currentPlayer = players.get(turn);
-	        show();
-	        int [] pos = currentPlayer.move(this, turn);
-	        if(placeMark(pos, turn))
-	        	break;
-	    }
-
-	    //swap turn
-	    turn = (turn == 'X')? 'O' : 'X';
-	}
-	
 	public static void main(String[] args) {
-		Game game = new Game();
-		game.run();
+		Player h = new HumanPlayer("aya"), c = new SuperComputerPlayer("ali");
+		Game game = new Game(h, c);
 	}
-
 }
