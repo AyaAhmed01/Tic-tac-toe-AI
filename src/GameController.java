c443b49c723705cdb99b6a2194399ea150ca6807
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-22 22:03:01 +0200

    Initial commit

---
+++ src/GameController.java
@@ -0,0 +1,80 @@
+import javax.swing.JButton;
+
+public class GameController {
+	
+	private Game game;
+	private GameView view;
+	
+	public GameController() {
+		this.game = new Game();
+		this.view = new GameView();
+		initView();
+		initController();
+	}
+	
+	private void initController() {
+		JButton [][] buttons = view.getButtons();
+		for(int i = 0; i < 3; i++) 
+			for(int j = 0; j < 3; j++) {
+				int [] btnPos = new int [] {i, j};
+				buttons[i][j].addActionListener(e -> placeMark(btnPos)); 
+			}
+		run();
+    }
+	
+	private void placeMark(int [] btnPos) {
+		if(game.placeBoardMark(btnPos) && game.currentPlayer() instanceof HumanPlayer) {
+			view.placeButtonMark(btnPos, game.turn);
+			swapTurns();
+		}
+	}
+	
+	private void displayTurn() {
+		view.viewText(game.turn + "'s turn");
+	}
+	
+	private void run() {
+		displayTurn();
+		while(!game.isOver()) {
+			if(!(game.currentPlayer() instanceof HumanPlayer)) 
+				playComputerTurn();
+		}
+		
+		if(game.isWon()){
+	        Player winningPlayer = game.winner();
+	        view.viewText(winningPlayer.name +" won the game!");
+	    }
+	    else {
+	        view.viewText("No one wins!");
+	    }
+	}
+	
+	void swapTurns() {
+	    game.swap();
+	    displayTurn();
+	}
+
+	private void playComputerTurn(){  
+		try {                                    
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		    return; 
+		    }
+        int [] pos = game.getMove(); 
+        if(game.placeBoardMark(pos))         
+        	view.placeButtonMark(pos, game.turn);     
+        swapTurns();                        
+	}
+	
+	private void initView() {
+		view.startFrame();
+		view.viewText("Amazing AI Tic Tac Toe!");
+		try {                                    
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		    return; 
+		    }
+	}	
+}
