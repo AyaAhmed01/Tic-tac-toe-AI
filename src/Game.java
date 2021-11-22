6f4299a2095024c3e1458ac42b8e85d749f60976
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-22 22:11:45 +0200

    Move playComputerTurn() and placeBoardMark() to GameController and Add board state getter methods

--- src/Game.java
+++ src/Game.java
@@ -1,106 +1,56 @@
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class Game {
 	
 	Board board;
-	HashMap<Character, Player> players;
-	Player currentPlayer;
+	private HashMap<Character, Player> players;
 	Character turn;
-	BoardView boardView;
 	
-	public Game (Player p1, Player p2){
+	public Game (){
 	    board = new Board();
 	    players = new HashMap<>();
 	    turn = 'X';
-	    boardView = new BoardView(this);
-//	    initializePlayers();               // -----------edit---------------
-		players.put('X', p1);
-		players.put('O', p2);
-	    newTurn();
+	    Player humPly = new HumanPlayer();
+	    Player comPly = new SuperComputerPlayer();
+	    players.put('X', humPly);
+	    players.put('O', comPly);
 	}
 	
-	private int getPlayerType(Character symbol) {
-		System.out.println("Please select type of "+ symbol +" player:");
-		System.out.println("1 for human player\n2 for computer player\n3 for super computer player");
-		try(Scanner sc = new Scanner(System.in)){
-			int type = sc.nextInt();
-			return type;
-		}
+	Player currentPlayer() {
+		return players.get(turn);
 	}
 	
-	private String getPlayerName(Character symbol) {
-		System.out.println("What is " + symbol + " player name");
-		try(Scanner sc = new Scanner(System.in)){
-			String name = sc.nextLine();
-			return name;
-		}
-	}
-
-	void newTurn(){
-	    if(board.isWon()){
-	        Player winningPlayer = players.get(board.winner());
-	        boardView.viewText("Congratulations " + winningPlayer.name +", you won the game!");
-	    }
-	    else if(board.isTied()){
-	    	boardView.viewText("No one wins!");
-	    }
-	    else {
-	    	boardView.viewText(turn + "'s turn");
-	    	currentPlayer = players.get(turn);
-	    	if(!(currentPlayer instanceof HumanPlayer))
-	    		playComputerTurn();
-	    }
-	}
-	
-	boolean placeBoardMark (int [] pos, Character mark) throws IllegalArgumentException {
+	boolean placeBoardMark (int [] pos) throws IllegalArgumentException {
 	    try{
-	        board.setMark(pos, mark);      // throws exception when cell is not empty
+	        board.setMark(pos, turn);      // throws exception when cell is not empty
 	        return true;
 	    }
 	    catch (IllegalArgumentException e){
-	    	boardView.viewText(e.getMessage());
 	        return false;
 	    }
 	}
 	
-	void swapTurns() {
-	    turn = (turn == 'X')? 'O' : 'X';
-	    newTurn();
+	int [] getMove() {
+		return currentPlayer().move(this, turn);
 	}
 
-	private void playComputerTurn(){
-        currentPlayer = players.get(turn);
-        int [] pos = currentPlayer.move(this, turn);
-        if(placeBoardMark(pos, turn)) {
-        	boardView.placeButtonMark(pos, turn);
-        	swapTurns();
-        }
-	}
-
-	private void initializePlayers() {
-		System.out.println("Welcome to the Amazing Tic Tac Toe!\n");
-		Character [] symbols = new Character[] {'X', 'O'};
-		for(Character symbol : symbols) {
-			int type = getPlayerType(symbol);
-			String name = getPlayerName(symbol);
-			Player player;
-			switch(type) {
-			case 1:
-				player = new HumanPlayer(name);
-				break;
-			case 2:
-				player = new ComputerPlayer(name);
-				break;
-			default:
-				player = new SuperComputerPlayer(name);
-			}
-			players.put(symbol, player);
-		}		
+	boolean isOver() {
+		return board.isOver();
 	}
 	
-	public static void main(String[] args) {
-		Player h = new HumanPlayer("aya"), c = new SuperComputerPlayer("ali");
-		Game game = new Game(h, c);
+	boolean isWon() {
+		return board.isWon();
+	}
+	
+	boolean isTied() {
+		return board.isTied();
+	}
+	
+	void swap() {
+		turn = (turn == 'X')? 'O' : 'X';
+	}
+	
+	Player winner() {
+		return players.get(board.winner());
 	}
 }
