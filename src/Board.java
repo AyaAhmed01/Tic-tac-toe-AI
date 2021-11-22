2f5ec95fd41c14740e09d083f0daf0c63597a564
Aya Ahmed <Aya.Ahmed01@eng-st.cu.edu.eg>
2021-11-22 23:02:11 +0200

    Modify access specifiers

--- src/Board.java
+++ src/Board.java
@@ -1,6 +1,6 @@
 public class Board {
 		
-		public Character[][] rows;
+		Character[][] rows;
 		
 		private static Character[][] blankGrid(){
 			Character [][] grid = new Character [3][3];
@@ -18,15 +18,15 @@ public class Board {
 			 this.rows = blankGrid();
 		}
 		
-		public boolean isEmpty(int [] pos) {
+		boolean isEmpty(int [] pos) {
 			return (getMark(pos) == '-');
 		}
 		
-		public Character getMark(int [] pos) {
+		Character getMark(int [] pos) {
 			return rows[pos[0]][pos[1]];
 		}
 		
-		public void setMark(int [] pos, Character mark) {
+		void setMark(int [] pos, Character mark) {
 			if(!isEmpty(pos))
 				throw new IllegalArgumentException("Non-empty cell, mark is already there!");
 			int row = pos[0], col = pos[1];
@@ -41,13 +41,13 @@ public class Board {
 			return newRows;
 		}
 		
-		public Board deepClone() {
+		Board deepClone() {
 			Character [][] rows = rowsClone();
 			Board board = new Board(rows);
 			return board;
 		}
 		
-	    public Character checkRowWinner(){
+	    private Character checkRowWinner(){
 	       for(int i = 0; i < 3; i++){
 	         if(rows[i][0] == rows[i][1] && rows[i][1] == rows[i][2] && !isEmpty(new int [] {i, 0}))
 	        	 return rows[i][0];
@@ -55,7 +55,7 @@ public class Board {
 	        return '-';
 	    }
 
-	    public Character checkColWinner(){
+	    private Character checkColWinner(){
 	       for(int i = 0; i < 3; i++){
 	         if(rows[0][i] == rows[1][i] && rows[1][i] == rows[2][i] && !isEmpty(new int [] {0, i}))
 	        	 return rows[0][i];
@@ -63,7 +63,7 @@ public class Board {
 	        return '-';
 	    }
 
-	   public Character checkDiagWinner(){
+	   private Character checkDiagWinner(){
 	       boolean leftDiagWinner = (rows[1][1] == rows[0][0] && rows[1][1] == rows[2][2]);
 	       boolean rightDiagWinner = (rows[1][1] == rows[0][2] && rows[1][1] == rows[2][0]);
 	       int [] centerPos = new int[] {1, 1};
@@ -75,7 +75,7 @@ public class Board {
 	       return '-';
 	   }
 
-	   public Character winner(){
+	   Character winner(){
 	       Character rowWinner = checkRowWinner(), colWinner = checkColWinner(), diagWinner = checkDiagWinner();
 	       if (rowWinner != '-')
 	         return rowWinner;
@@ -87,7 +87,7 @@ public class Board {
 	       return '-';
 	   }
 	   
-	   public boolean isTied(){
+	   boolean isTied(){
 	       if (isWon()) 
 	        return false;
 	       
@@ -101,31 +101,12 @@ public class Board {
 	       return true;
 	   }
 
-	   public boolean isWon(){
+	   boolean isWon(){
 	       return !(winner() == '-');
 	   }
 
-	   public boolean isOver(){
+	   boolean isOver(){
 	      return (isWon() || isTied());
 	   }
-	
-//	public static void main(String[] args) {
-//		Board brd = new Board();
-//		int [] pos1 = {0, 1};
-//		System.out.println(brd.isEmpty(0, 1));
-//		System.out.println(brd.getMark(0, 1));
-//		brd.setMark(0, 1, 'X');
-//		brd.setMark(0, 0, 'o');
-//		brd.setMark(0, 2, 'X');
-//		
-//		brd.setMark(1, 1, 'o');
-//		brd.setMark(1, 0, 'x');
-//		brd.setMark(1, 2, 'o');
-//		brd.setMark(2, 1, 'X');
-//		brd.setMark(2, 0, 'o');
-//		brd.setMark(2, 2, 'X');
-//		System.out.println(brd.isOver());
-//		System.out.println(brd.winner());
-//	}
 
 }
